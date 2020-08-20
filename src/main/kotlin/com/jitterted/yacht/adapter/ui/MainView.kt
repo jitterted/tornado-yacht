78784aebc4ec8cfe22c93c433f0742b128f78d09
Ted M. Young <tedyoung@gmail.com>
2020-08-20 15:17:52 -0700

    Generalized button display to take buttons from the ScoreCategory enum.
    Enabled/disabled the "roll dice" button based on the game's round completed state.

--- src/main/kotlin/com/jitterted/yacht/adapter/ui/MainView.kt
+++ src/main/kotlin/com/jitterted/yacht/adapter/ui/MainView.kt
@@ -2,6 +2,7 @@ package com.jitterted.yacht.adapter.ui
 
 import com.jitterted.yacht.domain.Game
 import com.jitterted.yacht.domain.ScoreCategory
+import javafx.beans.property.SimpleBooleanProperty
 import javafx.beans.property.SimpleIntegerProperty
 import javafx.beans.property.SimpleStringProperty
 import javafx.geometry.Insets
@@ -12,6 +13,7 @@ class MainView : View() {
     val game = Game()
     val rollProperty = SimpleStringProperty("no roll")
     val scoreProperty = SimpleIntegerProperty(0)
+    val canRollProperty = SimpleBooleanProperty(true)
 
     init {
         game.start()
@@ -36,29 +38,20 @@ class MainView : View() {
                         action {
                             game.rollDice()
                             rollProperty.value = game.lastRoll().toString()
+                            canRollProperty.value = game.roundCompleted()
                         }
+                        enableWhen(canRollProperty)
                     }
                 }
                 vbox {
                     padding = Insets(10.0)
-                    button("ONES") {
-                        action {
-                            game.assignRollTo(ScoreCategory.ONES)
-                            scoreProperty.value = game.score()
-                        }
-                    }
-                    button("TWOS")
-                    {
-                        action {
-                            game.assignRollTo(ScoreCategory.TWOS)
-                            scoreProperty.value = game.score()
-                        }
-                    }
-                    button("THREES")
-                    {
-                        action {
-                            game.assignRollTo(ScoreCategory.THREES)
-                            scoreProperty.value = game.score()
+                    ScoreCategory.values().forEach {
+                        button(it.toString()) {
+                            action {
+                                game.assignRollTo(it)
+                                scoreProperty.value = game.score()
+                                canRollProperty.value = game.roundCompleted()
+                            }
                         }
                     }
                 }
