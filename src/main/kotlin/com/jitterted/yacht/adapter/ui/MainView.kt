1d33c9f1ef0fdb8dea92a24dd87797855b0c6cc6
Ted M. Young <tedyoung@gmail.com>
2020-08-20 14:49:40 -0700

    Hooked up category assignment and score view.

--- src/main/kotlin/com/jitterted/yacht/adapter/ui/MainView.kt
+++ src/main/kotlin/com/jitterted/yacht/adapter/ui/MainView.kt
@@ -1,6 +1,8 @@
 package com.jitterted.yacht.adapter.ui
 
 import com.jitterted.yacht.domain.Game
+import com.jitterted.yacht.domain.ScoreCategory
+import javafx.beans.property.SimpleIntegerProperty
 import javafx.beans.property.SimpleStringProperty
 import tornadofx.*
 
@@ -8,17 +10,37 @@ class MainView : View() {
 
     val game = Game()
     val rollProperty = SimpleStringProperty("no roll")
+    val scoreProperty = SimpleIntegerProperty(0)
 
     init {
         game.start()
-        rollProperty.value = game.lastRoll().toString()
     }
 
     override val root =
-            vbox {
-                hbox {
-                    label(rollProperty)
+            hbox {
+                vbox {
+                    label(scoreProperty)
                 }
-                button("Roll Dice")
+                vbox {
+                    hbox {
+                        label(rollProperty)
+                    }
+                    button("Roll Dice") {
+                        action {
+                            game.rollDice()
+                            rollProperty.value = game.lastRoll().toString()
+                        }
+                    }
+                }
+                vbox {
+                    button("ONES") {
+                        action {
+                            game.assignRollTo(ScoreCategory.ONES)
+                            scoreProperty.value = game.score()
+                        }
+                    }
+                    button("TWOS")
+                    button("THREES")
+                }
             }
 }
